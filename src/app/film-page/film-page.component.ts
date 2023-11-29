0ef5af2dc04a9ab947e0a38077af6fc612d6d93a
DEROUICH KARIM <106762744+MUGUWARA1@users.noreply.github.com>
2023-11-29 11:15:37 +0100

    fix film page

---
+++ src/app/film-page/film-page.component.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-film-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './film-page.component.html',
+  styleUrl: './film-page.component.css'
+})
+export class FilmPageComponent {
+  @Input Nom :string|undefined;
+  @Input Annee :number|undefined;
+  @Input Description 
+
+}
