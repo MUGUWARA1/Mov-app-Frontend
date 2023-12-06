d8e0cf9cddf9f8f4780538a6650d7150ead840c3
DEROUICH KARIM <106762744+MUGUWARA1@users.noreply.github.com>
2023-12-06 09:19:37 +0100

    probelm of connection

--- src/app/list-film/list-film.component.ts
+++ src/app/list-film/list-film.component.ts
@@ -2,6 +2,7 @@ import { Component,OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardFilmComponent } from '../card-film/card-film.component';
 import { FilmPageComponent } from '../film-page/film-page.component';
+import { ApiService } from '../Services/api/api.service';
 
 
 @Component({
@@ -12,57 +13,10 @@ import { FilmPageComponent } from '../film-page/film-page.component';
   styleUrl: './list-film.component.css'
 })
 export class ListFilmComponent implements OnInit {
-  constructor(){
-
+   films: any;
+  constructor(private moviesServces : ApiService){}
+  ngOnInit() {
+    this.moviesServces.getMovie().subscribe((data : any)=>this.films=data.results);
   }
-  ngOnInit(): void {
-    throw new Error('Method not implemented.');
-  }
-  films=[{
-    id:1,
-    annee:2023,
-    Nom:"Mission Impossible",
-    imageURL:"https://dx35vtwkllhj9.cloudfront.net/paramountpictures/mission-impossible-7/images/regions/us/onesheet.jpg",
-    Desription :"jjhvdjhvfjvdfjvjdvfjvdjvfjvjfvsjdvfjdsvfjvsjdvdfj"
-
-  },{
-    id:2,
-    annee:2020,
-    Nom:"the equalizer",
-    imageURL:"./assets/theEqualizer.jpg"
-    ,
-    Desription :"jjhvdjhvfjvdfjvjdvfjvdjvfjvjfvsjdvfjdsvfjvsjdvdfj"
-  },
-  {
-    id:2,
-    annee:2020,
-    Nom:"Seven",
-    imageURL:"./assets/Seven.jpg",
-    Desription :"jjhvdjhvfjvdfjvjdvfjvdjvfjvjfvsjdvfjdsvfjvsjdvdfj"
-  },
-  {
-      id:3,
-      annee:1999,
-      Nom:"Forest Gump",
-      imageURL:"./assets/Seven.jpg"
-      ,
-    Desription :"jjhvdjhvfjvdfjvjdvfjvdjvfjvjfvsjdvfjdsvfjvsjdvdfj"
-    }
-    ,{
-      id:4,
-      annee:2020,
-      Nom:"Seven",
-      imageURL:"./assets/Seven.jpg"
-      ,
-    Desription :"jjhvdjhvfjvdfjvjdvfjvdjvfjvjfvsjdvfjdsvfjvsjdvdfj"
-    }
-    ,{
-      id:5,
-      annee:2020,
-      Nom:"Seven",
-      imageURL:"./assets/Seven.jpg"}
-  ]
-
-
-
+  
 }
